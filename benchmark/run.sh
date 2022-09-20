b0ee295b7e475b8991f38f13e0a9847c9aff1cea
ZhiYong Wang <botu.wzy@antgroup.com>
2022-09-20 16:46:57 +0800

    update

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -6,32 +6,26 @@ export LD_LIBRARY_PATH=../build/output:$LD_LIBRARY_PATH
 #./simple_paper_wal $*
 
 
-:<<!
-rm lgraph_data -rf
-./simple_paper_wal --durable=1 --mode=batch --vertex_num=1000000 --batch_num=10
 
-rm lgraph_data -rf
-./simple_paper_wal --durable=0 --mode=batch --vertex_num=1000000 --batch_num=10
-!
-
-:<<!
 rm lgraph_data -rf
 ./simple_paper_wal --durable=1 --vertex_num=1000000
-
 rm lgraph_data -rf
 ./simple_paper_wal --durable=0 --vertex_num=1000000
-!
 
-:<<!
+
+rm lgraph_data -rf
+./simple_paper_wal --durable=1 --mode=batch --vertex_num=1000000 --batch_num=10
+rm lgraph_data -rf
+./simple_paper_wal --durable=0 --mode=batch --vertex_num=1000000 --batch_num=10
+
+
 rm lgraph_data -rf
 ./simple_paper_wal --durable=1 --mode=multi_thread --vertex_num=1000000 --thread_num=10 --batch_num=1
-
 rm lgraph_data -rf
 ./simple_paper_wal --durable=0 --mode=multi_thread --vertex_num=1000000 --thread_num=10 --batch_num=1
-!
+
 
 rm lgraph_data -rf
 ./simple_paper_wal --durable=1 --mode=multi_thread --vertex_num=1000000 --thread_num=10 --batch_num=10
-
 rm lgraph_data -rf
 ./simple_paper_wal --durable=0 --mode=multi_thread --vertex_num=1000000 --thread_num=10 --batch_num=10
