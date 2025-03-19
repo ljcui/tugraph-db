b8957ac25c5890b6862afa3843b056e355503d5d
Wang Zhiyong <zhiyong4wang@foxmail.com>
2025-03-19 09:23:07 +0000

    rename class

--- vector_store/vector_index_store.cpp
+++ vector_store/vector_index_store.cpp
@@ -33,6 +33,27 @@ struct IndexFileInfo {
     std::string segment;
 };
 
+void MutableVectorIndex::Add(const vsag::DatasetPtr &dataset) {
+    auto result = index()->Add(dataset);
+    if (result.has_value()) {
+        if (!result.value().empty()) {
+            THROW_CODE(VectorIndexException,
+                       "failed to insert {} ids into vector index",
+                       result.value().size());
+        }
+    } else {
+        THROW_CODE(VectorIndexException, result.error().message);
+    }
+    for (auto i = 0; i < dataset->GetNumElements(); i++) {
+        ids_.push_back(*(dataset->GetIds() + i));
+        vectors_.emplace_back(dataset->GetFloat32Vectors() + dataset->GetDim()*i, dataset->GetFloat32Vectors() + dataset->GetDim()*(i+1));
+    }
+}
+
+void MutableVectorIndex::Delete(uint64_t vector_id) {
+    deletes()->add(vector_id);
+}
+
 void VectorIndexStore::LoadIndex() {
     std::string meta_file;
     std::unordered_map<uint64_t, IndexFileInfo> files;
@@ -61,14 +82,14 @@ void VectorIndexStore::LoadIndex() {
     meta_fd >> meta;
     meta_fd.close();
     metaInfo_.applied_id = meta["applied_id"].get<uint64_t>();
-    metaInfo_.ids = meta["ids"].get<std::vector<uint64_t>>();
+    metaInfo_.ids = meta["ids"].get<std::set<uint64_t>>();
     for (auto id : metaInfo_.ids) {
         next_index_id_ = std::max(next_index_id_, id);
         auto& info = files.at(id);
         if (info.is_hnsw) {
             // load hnsw index
             // [version][keys_size][key_len][key_data][index_len][index_data]...
-            std::ifstream index_fd(info.index, std::ios::in);
+            std::ifstream index_fd(info.index, std::ios::in|std::ios::binary);
             char version;
             size_t keys_size = 0;
             read_binary_pod(index_fd, version);
@@ -86,8 +107,17 @@ void VectorIndexStore::LoadIndex() {
                 bs.Set(key, b);
             }
             index_fd.close();
-            auto index = NewEmptyIndex(id);
-            index->index()->Deserialize(bs);
+
+            std::shared_ptr<vsag::Index> index;
+            auto ret = vsag::Factory::CreateIndex("hnsw", options_.index_build_parameters);
+            if (ret.has_value()) {
+                index = ret.value();
+            } else {
+                THROW_CODE(VectorIndexException, "Failed to create vector index");
+            }
+
+            //auto index = NewEmptyIndex(id);
+            index->Deserialize(bs);
             // load deletes
             std::ifstream deletes_fd(info.deletes, std::ios::in|std::ios::binary);
             deletes_fd.seekg(0, std::ios::end);
@@ -95,8 +125,9 @@ void VectorIndexStore::LoadIndex() {
             deletes_fd.seekg(0, std::ios::beg);
             std::vector<char> buffer(size);
             read_binary(deletes_fd, buffer.data(), size);
-            *index->deletes() = roaring::Roaring64Map::read(buffer.data(), false);
-            immutable_indexes_.emplace_back(std::move(index));
+            auto deletes = std::make_shared<roaring::Roaring64Map>(roaring::Roaring64Map::read(buffer.data(), false));
+            auto persistent = std::make_shared<PersistentVectorIndex>(id, index, deletes, info.index, info.deletes, info.segment);
+            persistent_indexes_[persistent->index_id()] = persistent;
         }
     }
     next_index_id_++;
@@ -108,23 +139,29 @@ VectorIndexStore::VectorIndexStore(const std::string& path, const IndexStoreOpti
     if (!std::filesystem::exists(path)) {
         std::filesystem::create_directories(path_);
         std::string meta_path = fmt::format("{}/meta.json", path_);
-        std::ofstream meta_fd(meta_path, std::ios::binary);
+        std::ofstream meta_fd(meta_path, std::ios::out|std::ios::binary);
         meta_fd << metaInfo_.Serialize();
         meta_fd.close();
     }
     LoadIndex();
     auto parameters = nlohmann::json::parse(options_.index_build_parameters);
     dim_ = parameters["dim"].get<int64_t>();
-    mutable_index_ = NewEmptyIndex(next_index_id_++);
+    mutable_index_ = NewEmptyMutableVectorIndex(next_index_id_++);
     threads_.emplace_back([this]() {
         pthread_setname_np(pthread_self(), "flush_service");
         boost::asio::io_service::work holder(flush_service_);
         flush_service_.run();
     });
+    threads_.emplace_back([this]() {
+        pthread_setname_np(pthread_self(), "merge_service");
+        boost::asio::io_service::work holder(merge_service_);
+        merge_service_.run();
+    });
 }
 
 VectorIndexStore::~VectorIndexStore() {
     flush_service_.stop();
+    merge_service_.stop();
     for (auto& t : threads_) {
         t.join();
     }
@@ -136,36 +173,17 @@ void VectorIndexStore::Add(int64_t id, const float* vectors, uint64_t apply_id) 
     ids[0] = id;
     auto dataset = vsag::Dataset::Make();
     dataset->Dim(dim_)->NumElements(1)->Ids(ids)->Float32Vectors(vectors);
+    std::unique_lock<std::shared_mutex> write_lock(rw_mutex_);
     mutable_index_->Add(dataset);
-    if (mutable_index_->Size() >= 10000) {
-        mutable_index_->Freeze(apply_id);
-        immutable_deletes_[mutable_index_->index_id()] = mutable_index_->deletes();
-        immutable_indexes_.push_back(mutable_index_);
-        boost::asio::post(flush_service_, [this, index = mutable_index_](){
-            Flush(index);
+    if (mutable_index_->Size() >= options_.mutable_index_max_size) {
+        auto immutable = std::make_shared<ImmutableVectorIndex>(apply_id, std::move(*mutable_index_));
+        immutable_indexes_[mutable_index_->index_id()] = immutable;
+        immutable_deletes_[mutable_index_->index_id()] = immutable->deletes();
+        boost::asio::post(flush_service_, [this, immutable](){
+            Flush(immutable);
         });
-        mutable_index_ = NewEmptyIndex(next_index_id_++);
+        mutable_index_ = NewEmptyMutableVectorIndex(next_index_id_++);
     }
-}
-
-void VectorIndex::Add(const vsag::DatasetPtr& dataset) {
-    std::unique_lock<std::shared_mutex> lock(rw_mutex_);
-    auto result = index_->Add(dataset);
-    if (result.has_value()) {
-        if (!result.value().empty()) {
-            THROW_CODE(VectorIndexException,
-                       "failed to insert {} ids into vector index",
-                       result.value().size());
-        }
-    } else {
-        THROW_CODE(VectorIndexException, result.error().message);
-    }
-    vectors_.emplace_back(dataset->GetFloat32Vectors(), dataset->GetFloat32Vectors() + dataset->GetDim());
-}
-
-void VectorIndex::Delete(uint64_t vector_id) {
-    std::unique_lock<std::shared_mutex> lock(rw_mutex_);
-    deletes_->add(vector_id);
 }
 
 std::string MetaInfo::Serialize() {
@@ -175,20 +193,126 @@ std::string MetaInfo::Serialize() {
     return obj.dump(4);
 }
 
-std::shared_ptr<VectorIndex> VectorIndexStore::NewEmptyIndex(uint64_t index_id) {
+std::shared_ptr<MutableVectorIndex> VectorIndexStore::NewEmptyMutableVectorIndex(uint64_t index_id) const {
     auto ret = vsag::Factory::CreateIndex("hnsw", options_.index_build_parameters);
     if (ret.has_value()) {
-        return std::make_shared<VectorIndex>(index_id, std::move(ret.value()));
+        return std::make_shared<MutableVectorIndex>(index_id, std::move(ret.value()));
     } else {
         THROW_CODE(VectorIndexException, "Failed to create vector index");
     }
 }
 
-void VectorIndexStore::Flush(const std::shared_ptr<VectorIndex>& index) {
+void VectorIndexStore::MaybeMergeIndex() {
+    std::vector<std::shared_ptr<PersistentVectorIndex>> indexes;
+    for (auto& [_, index] : persistent_indexes_) {
+        if (index->Size() < 100000) {
+            indexes.emplace_back(index);
+            if (indexes.size() == 2) {
+                break;
+            }
+        }
+    }
+    if (indexes.size() != 2) {
+        return;
+    }
+    std::shared_ptr<vsag::Index> new_index;
+    auto new_deletes = std::make_shared<roaring::Roaring64Map>(*indexes[0]->deletes() | *indexes[1]->deletes());
+    auto new_index_id = next_index_id_++;
+    std::string new_segment_path = fmt::format("{}/{}.segment", path_, new_index_id);
+    std::string new_index_path = fmt::format("{}/{}.hnsw", path_, new_index_id);
+    std::string new_deletes_path = fmt::format("{}/{}.deletes", path_, new_index_id);
+    std::ofstream new_segment_fd(new_segment_path, std::ios::out | std::ios::binary);
+    auto ret = vsag::Factory::CreateIndex("hnsw", options_.index_build_parameters);
+    if (ret.has_value()) {
+        new_index = ret.value();
+    } else {
+        THROW_CODE(VectorIndexException, "Failed to create vector index");
+    }
+
+    for (auto& index: indexes) {
+        std::ifstream fd(index->segment_path(), std::ios::in | std::ios::binary);
+        auto num = index->index()->GetNumElements();
+        while (num > 0) {
+            int64_t batch = std::min((int64_t) 1000, num);
+            auto *ids = new int64_t[batch];
+            auto *floats = new float[batch * dim_];
+            for (auto i = 0; i < batch; i++) {
+                read_binary_pod(fd, ids[i]);
+                read_binary(fd, (char*)(floats + dim_ * i), dim_* sizeof(float));
+                write_binary_pod(new_segment_fd, ids[i]);
+                write_binary(new_segment_fd, (char*)(floats + dim_ * i), dim_* sizeof(float));
+            }
+            auto dataset = vsag::Dataset::Make();
+            dataset->Dim(dim_)->NumElements(batch)->Ids(ids)->Float32Vectors(floats);
+            new_index->Add(dataset);
+            num -= batch;
+        }
+    }
+    new_segment_fd.close();
+
+    auto size = new_deletes->getSizeInBytes(false);
+    std::vector<char> buffer(size);
+    new_deletes->write(buffer.data(), false);
+    std::ofstream new_deletes_fd(new_deletes_path, std::ios::out|std::ios::binary);
+    write_binary(new_deletes_fd, buffer.data(), size);
+    new_deletes_fd.close();
+
+    if (auto bs = new_index->Serialize(); bs.has_value()) {
+        std::ofstream new_index_fd(new_index_path, std::ios::out|std::ios::binary);
+        auto keys = bs->GetKeys();
+        // [version][keys_size][key_len][key_data][index_len][index_data]...
+        char version = 1;
+        write_binary_pod(new_index_fd, version);
+        write_binary_pod(new_index_fd, keys.size());
+        for (auto& key : keys) {
+            write_binary_pod(new_index_fd, key.size());
+            write_binary(new_index_fd, key.data(), key.size());
+            vsag::Binary b = bs->Get(key);
+            write_binary_pod(new_index_fd, b.size);
+            write_binary(new_index_fd, (const char*)b.data.get(), b.size);
+        }
+        new_index_fd.close();
+    } else if (bs.error().type == vsag::ErrorType::NO_ENOUGH_MEMORY) {
+        LOG_ERROR("no enough memory to serialize index {}", new_index_path);
+        return;
+    } else {
+        LOG_ERROR("failed to serialize index {}", new_index_path);
+        return;
+    }
+
+    // update meta info
+    metaInfo_.ids.insert(new_index_id);
+    metaInfo_.ids.erase(indexes[0]->index_id());
+    metaInfo_.ids.erase(indexes[1]->index_id());
+    std::string meta_path = fmt::format("{}/meta.json", path_);
+    std::string tmp_meta_path = fmt::format("{}/meta.json.tmp", path_);
+    std::ofstream tmp_meta_fd(tmp_meta_path, std::ios::out);
+    tmp_meta_fd << metaInfo_.Serialize();
+    tmp_meta_fd.close();
+    std::filesystem::rename(tmp_meta_path, meta_path);
+
+    {
+        std::unique_lock<std::shared_mutex> write_lock(rw_mutex_);
+        persistent_indexes_.erase(indexes[0]->index_id());
+        persistent_indexes_.erase(indexes[1]->index_id());
+        auto persistent = std::make_shared<PersistentVectorIndex>(new_index_id, new_index, new_deletes,
+                                                                  new_index_path, new_deletes_path, new_segment_path);
+        persistent_indexes_[persistent->index_id()] = persistent;
+    }
+
+    std::filesystem::remove(indexes[0]->index_path());
+    std::filesystem::remove(indexes[0]->deletes_path());
+    std::filesystem::remove(indexes[0]->segment_path());
+    std::filesystem::remove(indexes[1]->index_path());
+    std::filesystem::remove(indexes[1]->deletes_path());
+    std::filesystem::remove(indexes[1]->segment_path());
+}
+
+void VectorIndexStore::Flush(const std::shared_ptr<ImmutableVectorIndex>& index) {
     // flush index
     std::string index_path = fmt::format("{}/{}.hnsw", path_, index->index_id());
     if (auto bs = index->index()->Serialize(); bs.has_value()) {
-        std::ofstream index_fd(index_path, std::ios::binary);
+        std::ofstream index_fd(index_path, std::ios::out|std::ios::binary);
         auto keys = bs->GetKeys();
         // [version][keys_size][key_len][key_data][index_len][index_data]...
         char version = 1;
@@ -215,7 +339,7 @@ void VectorIndexStore::Flush(const std::shared_ptr<VectorIndex>& index) {
     auto size = index->deletes()->getSizeInBytes(false);
     std::vector<char> buffer(size);
     index->deletes()->write(buffer.data(), false);
-    std::ofstream deletes_fd(deletes_path, std::ios::binary);
+    std::ofstream deletes_fd(deletes_path, std::ios::out|std::ios::binary);
     if (!deletes_fd) {
         LOG_ERROR("failed to open {}", deletes_path);
         return;
@@ -225,21 +349,35 @@ void VectorIndexStore::Flush(const std::shared_ptr<VectorIndex>& index) {
 
     // flush segment
     std::string segment_path = fmt::format("{}/{}.segment", path_, index->index_id());
-    std::ofstream segment_fd(segment_path, std::ios::binary);
-    for (auto& vector : index->vectors()) {
-        write_binary(segment_fd, reinterpret_cast<const char *>(vector.data()), vector.size() * sizeof(float));
+    std::ofstream segment_fd(segment_path, std::ios::out|std::ios::binary);
+    auto num = index->ids().size();
+    for (size_t i = 0; i < num; i++) {
+        // [id][index][id][index]...
+        write_binary_pod(segment_fd, index->ids()[i]);
+        write_binary(segment_fd, reinterpret_cast<const char *>(index->vectors()[i].data()), index->vectors()[i].size() * sizeof(float));
     }
     segment_fd.close();
 
     // update meta info
     metaInfo_.applied_id = index->apply_id();
-    metaInfo_.ids.push_back(index->index_id());
+    metaInfo_.ids.insert(index->index_id());
     std::string meta_path = fmt::format("{}/meta.json", path_);
     std::string tmp_meta_path = fmt::format("{}/meta.json.tmp", path_);
-    std::ofstream tmp_meta_fd(tmp_meta_path, std::ios::binary);
+    std::ofstream tmp_meta_fd(tmp_meta_path, std::ios::out);
     tmp_meta_fd << metaInfo_.Serialize();
     tmp_meta_fd.close();
     std::filesystem::rename(tmp_meta_path, meta_path);
+
+    {
+        std::unique_lock<std::shared_mutex> write_lock(rw_mutex_);
+        immutable_indexes_.erase(index->index_id());
+        auto persistent = std::make_shared<PersistentVectorIndex>(
+                std::move(*index), index_path, deletes_path,segment_path);
+        persistent_indexes_[persistent->index_id()] = persistent;
+    }
+    boost::asio::post(flush_service_, [this](){
+        MaybeMergeIndex();
+    });
 }
 
 std::vector<std::pair<int64_t, float>> VectorIndexStore::KnnSearch(const float *query, int64_t top_k, int ef_search) {
@@ -262,8 +400,9 @@ std::vector<std::pair<int64_t, float>> VectorIndexStore::KnnSearch(const float *
             decltype(cmp)
     > merge(cmp);
 
-    std::shared_lock<std::shared_mutex> read_lock(mutable_index_->rw_mutex());
-    for (auto& index : immutable_indexes_) {
+    std::shared_lock<std::shared_mutex> read_lock(rw_mutex_);
+    // search persistent indexes
+    for (auto& [_, index] : persistent_indexes_) {
         auto result = index->index()->KnnSearch(dataset, top_k, param_str, [this](int64_t id)->bool {
             for (auto& [_, deletes] : immutable_deletes_) {
                 if (deletes->contains((uint64_t)id)) {
@@ -283,11 +422,31 @@ std::vector<std::pair<int64_t, float>> VectorIndexStore::KnnSearch(const float *
             THROW_CODE(VectorIndexException, result.error().message);
         }
     }
+    // search immutable indexes
+    for (auto& [_, index] : immutable_indexes_) {
+        auto result = index->index()->KnnSearch(dataset, top_k, param_str, [this](int64_t id)->bool {
+            for (auto& [_, deletes] : immutable_deletes_) {
+                if (deletes->contains((uint64_t)id)) {
+                    return true;
+                }
+            }
+            if (mutable_index_->deletes()->contains((uint64_t)id)) {
+                return true;
+            }
+            return false;
+        });
+        if (result.has_value()) {
+            for (int64_t i = 0; i < result.value()->GetDim(); i++) {
+                merge.emplace(result.value()->GetIds()[i], result.value()->GetDistances()[i]);
+            }
+        } else {
+            THROW_CODE(VectorIndexException, result.error().message);
+        }
+    }
+    // search mutable indexes
     auto result = mutable_index_->index()->KnnSearch(dataset, top_k, param_str, [this](int64_t id)->bool {
         return mutable_index_->deletes()->contains(uint64_t(id));
     });
-    read_lock.unlock();
-
     if (result.has_value()) {
         for (int64_t i = 0; i < result.value()->GetDim(); i++) {
             merge.emplace(result.value()->GetIds()[i], result.value()->GetDistances()[i]);
@@ -295,6 +454,7 @@ std::vector<std::pair<int64_t, float>> VectorIndexStore::KnnSearch(const float *
     } else {
         THROW_CODE(VectorIndexException, result.error().message);
     }
+    read_lock.unlock();
 
     std::vector<std::pair<int64_t, float>> final;
     while (!merge.empty() && final.size() < (size_t)top_k) {
@@ -305,12 +465,17 @@ std::vector<std::pair<int64_t, float>> VectorIndexStore::KnnSearch(const float *
 }
 
 void VectorIndexStore::Delete(uint64_t id) {
+    std::unique_lock<std::shared_mutex> write_lock(rw_mutex_);
     mutable_index_->Delete(id);
 }
 
 int64_t VectorIndexStore::VectorNum() {
+    std::shared_lock<std::shared_mutex> read_lock(rw_mutex_);
     int64_t num = 0;
-    for (auto& index : immutable_indexes_) {
+    for (auto& [_, index] : persistent_indexes_) {
+        num += index->index()->GetNumElements();
+    }
+    for (auto& [_, index] : immutable_indexes_) {
         num += index->index()->GetNumElements();
     }
     num += mutable_index_->index()->GetNumElements();
@@ -318,8 +483,12 @@ int64_t VectorIndexStore::VectorNum() {
 }
 
 uint64_t VectorIndexStore::DeleteNum() {
+    std::shared_lock<std::shared_mutex> read_lock(rw_mutex_);
     uint64_t num = 0;
-    for (auto& index : immutable_indexes_) {
+    for (auto& [_, index] : persistent_indexes_) {
+        num += index->deletes()->cardinality();
+    }
+    for (auto& [_, index] : immutable_indexes_) {
         num += index->deletes()->cardinality();
     }
     num += mutable_index_->deletes()->cardinality();
--- vector_store/vector_index_store.h
+++ vector_store/vector_index_store.h
@@ -2,8 +2,79 @@
 #include <boost/asio/io_service.hpp>
 #include <thread>
 #include <shared_mutex>
+#include <utility>
 
 class VectorIndex {
+public:
+    VectorIndex(uint64_t index_id, std::shared_ptr<vsag::Index> index)
+            : index_id_(index_id), index_(std::move(index)) {
+        deletes_ = std::make_shared<roaring::Roaring64Map>();
+    }
+    VectorIndex(uint64_t index_id, std::shared_ptr<vsag::Index> index, std::shared_ptr<roaring::Roaring64Map> deletes)
+            : index_id_(index_id), index_(std::move(index)), deletes_(std::move(deletes)) {}
+    VectorIndex(VectorIndex&& index) = default;
+    std::shared_ptr<vsag::Index>& index() {return index_;}
+    std::shared_ptr<roaring::Roaring64Map>& deletes() {return deletes_;}
+    //std::vector<std::vector<float>>& vectors() {return vectors_;}
+    [[nodiscard]] uint64_t index_id() const {return index_id_;}
+    int64_t Size() {
+        return index_->GetNumElements();
+    }
+private:
+    uint64_t index_id_ = 0;
+    std::shared_ptr<vsag::Index> index_;
+    std::shared_ptr<roaring::Roaring64Map> deletes_;
+};
+
+class MutableVectorIndex : public VectorIndex {
+public:
+    MutableVectorIndex(uint64_t index_id, std::shared_ptr<vsag::Index> index)
+        : VectorIndex(index_id, std::move(index)) {}
+    MutableVectorIndex(MutableVectorIndex&& index) = default;
+    void Add(const vsag::DatasetPtr& dataset);
+    void Delete(uint64_t vector_id);
+    const std::vector<int64_t>& ids() {return ids_;}
+    const std::vector<std::vector<float>>& vectors() {return vectors_;}
+private:
+    std::vector<int64_t> ids_;
+    std::vector<std::vector<float>> vectors_;
+};
+
+class ImmutableVectorIndex : public MutableVectorIndex {
+public:
+    ImmutableVectorIndex(uint64_t apply_id, MutableVectorIndex&& index)
+        : MutableVectorIndex(std::move(index)), apply_id_(apply_id) {}
+    [[nodiscard]] uint64_t apply_id() const {return apply_id_;}
+private:
+    uint64_t apply_id_ = 0;
+};
+
+class PersistentVectorIndex : public VectorIndex {
+public:
+    PersistentVectorIndex(ImmutableVectorIndex&& index, std::string  index_path,
+                          std::string deletes_path, std::string segment_path)
+        : VectorIndex(std::move(index)), index_path_(std::move(index_path)),
+        deletes_path_(std::move(deletes_path)), segment_path_(std::move(segment_path)) {}
+    PersistentVectorIndex(uint64_t index_id,
+                          std::shared_ptr<vsag::Index> index,
+                          std::shared_ptr<roaring::Roaring64Map> deletes,
+                          std::string index_path,
+                          std::string deletes_path,
+                          std::string segment_path)
+        : VectorIndex(index_id, std::move(index), std::move(deletes)),
+        index_path_(std::move(index_path)),
+        deletes_path_(std::move(deletes_path)),
+        segment_path_(std::move(segment_path)) {}
+    const std::string& segment_path() {return segment_path_;}
+    const std::string& index_path() {return index_path_;}
+    const std::string& deletes_path() {return deletes_path_;}
+private:
+    std::string index_path_;
+    std::string deletes_path_;
+    std::string segment_path_;
+};
+
+/*class VectorIndex {
 public:
     VectorIndex(int index_id, std::shared_ptr<vsag::Index> index)
         : index_id_(index_id), index_(std::move(index)) {
@@ -18,24 +89,27 @@ public:
         apply_id_ = apply_id;
         immutable_ = true;
     }
+    void SetPersistent() {
+        persistent_ = true;
+    }
+    [[nodiscard]] bool persistent() const {return persistent_;}
     [[nodiscard]] int index_id() const {return index_id_;}
     [[nodiscard]] uint64_t apply_id() const {return apply_id_;}
     std::shared_ptr<vsag::Index>& index() {return index_;}
     std::shared_ptr<roaring::Roaring64Map>& deletes() {return deletes_;}
     std::vector<std::vector<float>>& vectors() {return vectors_;}
-    std::shared_mutex& rw_mutex() {return rw_mutex_;}
 private:
     int index_id_ = 0;
     bool immutable_ = false;
+    bool persistent_ = false;
     uint64_t apply_id_ = 0;
     std::shared_ptr<vsag::Index> index_;
-    std::shared_mutex rw_mutex_;
     std::shared_ptr<roaring::Roaring64Map> deletes_;
     std::vector<std::vector<float>> vectors_;
-};
+};*/
 
 struct MetaInfo {
-    std::vector<uint64_t> ids;
+    std::set<uint64_t> ids;
     uint64_t applied_id = 0;
     std::string Serialize();
 };
@@ -55,17 +129,21 @@ public:
     uint64_t DeleteNum();
     ~VectorIndexStore();
 private:
-    void Flush(const std::shared_ptr<VectorIndex>& index);
-    std::shared_ptr<VectorIndex> NewEmptyIndex(uint64_t index_id);
+    void Flush(const std::shared_ptr<ImmutableVectorIndex>& index);
+    void MaybeMergeIndex();
+    std::shared_ptr<MutableVectorIndex> NewEmptyMutableVectorIndex(uint64_t index_id) const;
     void LoadIndex();
     std::string path_;
     IndexStoreOptions options_;
     int64_t dim_;
     boost::asio::io_service flush_service_;
-    std::shared_ptr<VectorIndex> mutable_index_;
-    std::vector<std::shared_ptr<VectorIndex>> immutable_indexes_;
+    boost::asio::io_service merge_service_;
+    std::shared_ptr<MutableVectorIndex> mutable_index_;
+    std::unordered_map<uint64_t, std::shared_ptr<ImmutableVectorIndex>> immutable_indexes_;
+    std::unordered_map<uint64_t, std::shared_ptr<PersistentVectorIndex>> persistent_indexes_;
     std::unordered_map<uint64_t, std::shared_ptr<roaring::Roaring64Map>> immutable_deletes_;
     std::vector<std::thread> threads_;
     uint64_t next_index_id_ = 0;
     MetaInfo metaInfo_;
+    std::shared_mutex rw_mutex_;
 };
